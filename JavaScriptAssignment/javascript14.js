084da05f37314c6514c03d9a787ee0ede7ea9992
NitinVijayValvi <113893117+NitinVijayValvi@users.noreply.github.com>
2022-11-30 17:06:33 +0530

    Add files via upload

---
+++ JavaScriptAssignment/javascript14.js
@@ -0,0 +1,16 @@
+/*14. Write a JavaScript program to find the maximal difference between any two adjacent 
+elements of a given array of integers.
+*/
+
+function maximum_difference(arr) {
+	var max = -1;
+    var temp;
+	for (var i = 0; i < arr.length - 1; i++)
+      {
+		temp = Math.abs(arr[i] - arr[i + 1]);
+		max = Math.max(max, temp);
+	  }
+	return max;
+}
+
+console.log(maximum_difference([4,7,14,15,20,22]));
