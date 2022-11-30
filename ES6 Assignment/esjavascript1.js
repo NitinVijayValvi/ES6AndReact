f0a1b09a6a6f43a7109141633434b290f8bd5f32
NitinVijayValvi <113893117+NitinVijayValvi@users.noreply.github.com>
2022-11-30 17:41:32 +0530

    Add files via upload

---
+++ ES6 Assignment/esjavascript1.js
@@ -0,0 +1,25 @@
+/* 1) Create an arrow function which will take multiple inputs and you have to print he
+minimum number from it. */
+var temp;
+var min = function (x, y, z) {
+    {
+        if (x < y) {
+            if (x < z) {
+                temp = x;
+            }
+            else {
+                temp = z;
+            }
+        }
+        else {
+            if (y < z) {
+                temp = y;
+            }
+            else {
+                temp = z;
+            }
+        }
+        return temp;
+    }
+};
+console.log(min(20, 30, 40));
