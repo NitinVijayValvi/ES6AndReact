3f155898c2137d0b8318c037ad19047f2ab48341
NitinVijayValvi <113893117+NitinVijayValvi@users.noreply.github.com>
2022-11-30 18:16:41 +0530

    Add files via upload

---
+++ ES6 Assignment/esjavascript5.js
@@ -0,0 +1,30 @@
+/*5) Create an arrow function by which you will take input of string and you check given 
+string is palindrome or not.
+*/
+
+function checkAlphanumeric(s) {
+    return s.match(/^[A-Za-z0-9]+$/);
+}
+var ispalindrome = function (s) {
+    var j = s.length - 1;
+    for (var i = 0; i < j; i++, j--) {
+        while (i < j && !checkAlphanumeric(s[i])) {
+            i++;
+        }
+        while (i < j && !checkAlphanumeric(s[j])) {
+            j--;
+        }
+        if (i < j && s[i].toLowerCase() !== s[j].toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
+};
+if (ispalindrome("nitin") == true) {
+    console.log("string is palindrome");
+}
+else {
+    console.log("not palindrome");
+}
+
+
